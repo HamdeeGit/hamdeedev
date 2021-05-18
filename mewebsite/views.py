c59992d48e9e239ce711e0f16a3852e835d89366
hamdee Web Dev <hamdeesalih@gmail.com>
2021-05-18 13:41:23 +0200

    Initial Commit

---
+++ mewebsite/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+def contact(request):
+    if request.method == "POST":
+        message_name_first = request.POST['message-name-first']
+        message_name_last = request.POST['message-name-last']
+        message_email_sender = request.POST['message-email-sender']
+        mobil_No_sender = request.POST['mobil-No-sender']
+        message = request.POST['message']
+        
+        # Send an Email
+        send_mail(
+            message_name_first, # Subject
+            message, # Message
+            message_email_sender, # From_Email
+            ['hamdeesalih@gmail.com', 'hamdee30@gmail.com'],      # To-Email-Target
+        )
+        return render(request, 'contact.html', { 'message_name_first': message_name_first })
+    else:
+        return render(request, 'contact.html', {})
+    
---
+++ static/mewebsite/js/script-main.js
@@ -0,0 +1,88 @@
+// JavaScript Document
+//  $(document).ready(function() {
+//     $('#autoWidth').lightSlider({
+//         autoWidth:true,
+//         auto:true,
+//         loop:true,
+//         pauseOnHover: true,
+//         onSliderLoad: function() {
+//             $('#autoWidth').removeClass('cS-hidden');
+//         } 
+//     });  
+//   });
+
+
+  // Skill Section 
+  // Try One
+  // $('.progress::before').each(function(){
+  //   var $this = $this;
+  //   var per = $this.attr('per');
+  //   $this.css("width",per+'%');
+  //   $({animatedValue: 0}).animate({animatedValue: per},{
+  //     duration: 1000,
+  //     step: function(){
+  //       $this.attr('per', Math.floor(this.animatedValue) + '%');
+  //     },
+  //     complete: function(){
+  //       $this.attr('per', Math.floor(this.animatedValue) + '%');
+  //     }
+  //   });
+  // });
+
+  //Try Two
+  // const progress = document.querySelector(".progress::before");
+  // const per = progress.getAttribute("per");
+
+
+  
+  // for(i=0; i<=per; i++){
+  //   console.log(i)
+  //   progress.style.content="i";
+  //   console.log(progress.style);
+  // }
+  
+  
+
+
+
+
+  // Contact Section
+
+  let contactInfo = document.querySelector('.contact-info');
+  let contactForm = document.querySelector('.contact-form');
+
+  let contactToggler=document.querySelector('#toggler-contact');
+  let messageToggler=document.querySelector('#toggler-message');
+  
+  contactToggler.addEventListener("click", function(){
+    messageToggler.checked = false;
+  })
+
+  messageToggler.addEventListener("click", function(){
+    contactToggler.checked = false;
+  })
+
+  // Header 
+
+  let header = document.querySelector('header');
+
+  window.addEventListener('scroll', function () {
+      let windowPosition = window.scrollY > 300;
+      header.classList.toggle('active', windowPosition)
+  })
+
+
+  let menuR = document.querySelector('.menuR');
+
+  window.addEventListener('scroll', function () {
+      let windowPosition = window.scrollY > 300;
+      menuR.classList.toggle('active', windowPosition)
+  })
+
+  // let section = document.querySelector('section');
+
+  // section.addEventListener('scroll', function () {
+  //     let sectionPosition = section.scrollY < 300;
+  //     section.classList.toggle('active', sectionPosition)
+  //     menuR.classList.toggle('active', windowPosition)
+  // })
